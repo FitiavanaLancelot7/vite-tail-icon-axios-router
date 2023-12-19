e1402e3ad5ef06973fa0fc5b7a7559af1d073192
Fitiavana Lancelot <fitiavanalancelot0@gmail.com>
2023-12-19 13:35:44 +0300

    css

--- src/Poster.jsx
+++ src/Poster.jsx
@@ -1,13 +1,17 @@
 import { MdOutlineFormatQuote } from "react-icons/md";
+import { MdDateRange } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
 const Poster = ({id, title, poster_path, popularity, genre_ids, backdrop_path, release_date, overview}) => {
   return (
     <div className="flex gap-1 mt-4 bg-[#fff4] rounded-md p-4">
-      <div className="">
+      <div className="flex items-center justify-center">
         <img className="h-[300px] object-cover rounded-md hover:scale-[1.05] transition-transform " src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
       </div>
       <div className="w-[15rem]">
-        <h1 className="ml-2"><u className="text-slate-950">Title :</u> <span className="text-slate-50">{title}</span></h1>
-        <h1 className="ml-2"><u className="text-slate-950">Descritpion :</u> <span className="text-slate-50"><MdOutlineFormatQuote className="rotate-180" />{overview}<MdOutlineFormatQuote /></span></h1>
+        <h1 className="ml-2 mb-[.7rem]"><u className="font-bold text-slate-950">Title :</u> <span className="text-slate-50">{title}</span></h1>
+        <h1 className="ml-2"><u className="font-bold text-slate-950">Descritpion :</u> <span className="text-slate-50"><MdOutlineFormatQuote className="rotate-180 text-yellow-600" />{overview}<MdOutlineFormatQuote className="text-yellow-600" /></span></h1>
+        <h1 className="ml-2"><u className="font-bold text-slate-950">Popularity :</u> <FaStar className="text-yellow-400" /><span className="relative top-[-1.25rem] left-[1.2rem]">{popularity}</span></h1>
+        <h1 className="ml-2"><u className="font-bold text-slate-950">Release date :</u> <MdDateRange/> <span className="relative top-[-1.25rem] left-[1.2rem] text-slate-200">{release_date.replaceAll("-", "/")}</span></h1>
       </div>
     </div>
   )
--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,7 +1,7 @@
 import Poster from "./Poster"
 const Tour = ({info}) => {
   return (
-    <div className="flex bg-gradient-to-r  from-slate-950 to-slate-800 flex-col p-4">
+    <div className="flex bg-gradient-to-tl  from-violet-700 to-violet-500 flex-col p-4">
       {
         info.map(dt=>{
           // const {id, title, poster_path, popularity, genre_ids, backdrop_path, release_date} = dt
